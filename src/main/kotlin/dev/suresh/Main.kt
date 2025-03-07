05505acbe7949082903946000545eb210eeeb125
Suresh G <sureshg.g@gmail.com>
2025-03-06 16:48:12 -0800

    chore: dep updates

--- src/main/kotlin/dev/suresh/Main.kt
+++ src/main/kotlin/dev/suresh/Main.kt
@@ -1,13 +1,11 @@
 package dev.suresh
 
+import java.security.KeyStore
 import java.security.cert.X509Certificate
 import kotlin.io.path.Path
 
 fun main(args: Array<String>) {
-  println("Hello World! ${BuildConfig.version}")
-
-  println("Supported Truststores")
-  TrustStore.allTrustStores().forEach { println(it) }
+  println("TrustStore Scan: ${BuildConfig.version}")
 
   // /var/lib/certs/*.jks
   // -Djavax.net.ssl.trustStore=repleo.jks -Djavax.net.ssl.trustStorePassword=changeit
@@ -17,23 +15,27 @@ fun main(args: Array<String>) {
   println("All CA from /etc/ssl/certs")
   val path = args.firstOrNull() ?: "/etc/ssl/certs"
   val keystore = TrustStore.systemTrustStore(TrustStoreType.Directory(Path(path)))
-  val aliases = keystore.aliases()
-  while (aliases.hasMoreElements()) {
-    val alias = aliases.nextElement()
-
-    if (keystore.isCertificateEntry(alias)) {
-      val cert = keystore.getCertificate(alias) as X509Certificate
-      println(
-          """
-                    |Certificate Alias: $alias
-                    |Subject: ${cert.subjectX500Principal}
-                    |Issuer: ${cert.issuerX500Principal}
-                    |Valid From: ${cert.notBefore}
-                    |Valid Until: ${cert.notAfter}
-                    |Serial Number: ${cert.serialNumber}
-                    |--------------------------------
-                """
-              .trimMargin())
-    }
+  keystore.certEntries.forEach {
+    println(
+        """
+        |Subject: ${it.subjectX500Principal}
+        |Serial Number: ${it.serialNumber}
+        |--------------------------------
+        """
+            .trimMargin())
   }
+
+  // println("Supported Truststores")
+  // TrustStore.allTrustStores().forEach { println(it) }
 }
+
+val KeyStore.certEntries
+  get() =
+      aliases()
+          .toList()
+          .filter { isCertificateEntry(it) }
+          .mapNotNull { getCertificate(it) }
+          .filterIsInstance<X509Certificate>()
+
+fun KeyStore.keyEntries(password: CharArray) =
+    aliases().toList().filter { isKeyEntry(it) }.mapNotNull { getKey(it, password) }
